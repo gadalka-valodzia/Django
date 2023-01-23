c56cdf00e5d7e5dd9480373c0501415587940b85
Vova <vovakr43@gmail.com>
2023-01-24 01:03:36 +0300

    Сделана таблица поощерения со связью,доработать

--- user/models.py
+++ user/models.py
@@ -7,9 +7,10 @@ from django.utils.text import slugify
 class Promotion(models.Model):
     promotion = models.CharField(max_length=40)
     personal_id = models.IntegerField(null=True)
-
     def __str__(self):
         return f'{self.promotion}'
+
+
 class User(models.Model):
     name = models.CharField(max_length=40)  # имя сотрудника
     surname = models.CharField(max_length=40)  # фамилия сотрудника
@@ -18,6 +19,6 @@ class User(models.Model):
     home_phone_number = models.IntegerField(blank=True)  # номер городского телефона сотрудника
     passport_number = models.IntegerField()  # номер паспорта
 
-    promotiom = models.ForeignKey(Promotion,on_delete=models.PROTECT,null=True)
+    promotion = models.ForeignKey(Promotion,on_delete=models.PROTECT,null=True)
 
 
